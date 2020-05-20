a449b6d058588589bcec61d0434a6a7b959fa49d
beka <mr.begzodjon96@mail.ru>
2020-05-21 01:42:44 +0300

    Creaeeted new tempeld  action

--- app/models/concerns/article.rb
+++ app/models/concerns/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
-
+   validates :title, presence: true, length: {minimum:3, maximum:15}
+   validates :description, presence: true, length: {minimum:10, maximum:300}
+   
+   
+   
 end
