75c5679e386f1775c843c6010022d478da90b5eb
beka <mr.begzodjon96@mail.ru>
2020-05-21 21:33:04 +0300

    Install  Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,22 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import $ from 'jquery';
+import 'bootstrap/dist/js/bootstrap';
+
+$(document).on('turbolinks:load', function() {
+  $('body').tooltip({
+    selector: '[data-toggle="tooltip"]',
+    container: 'body',
+  });
+
+  $('body').popover({
+    selector: '[data-toggle="popover"]',
+    container: 'body',
+    html: true,
+    trigger: 'hover',
+  });
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
-
+environment.loaders.append('expose', {
+  test: require.resolve('jquery'),
+  use: [
+    {
+      loader: 'expose-loader',
+      options: 'jQuery',
+    },
+    {
+      loader: 'expose-loader',
+      options: '$',
+    },
+  ],
+});
 module.exports = environment
