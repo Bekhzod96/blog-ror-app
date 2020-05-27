e56c54b09dc4e62232ab745895c0741899346a34
beka <mr.begzodjon96@mail.ru>
2020-05-27 11:09:37 +0300

    WebPack installed

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,3 @@
 const { environment } = require('@rails/webpacker')
-environment.loaders.append('expose', {
-  test: require.resolve('jquery'),
-  use: [
-    {
-      loader: 'expose-loader',
-      options: 'jQuery',
-    },
-    {
-      loader: 'expose-loader',
-      options: '$',
-    },
-  ],
-});
+
 module.exports = environment
