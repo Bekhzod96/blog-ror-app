21a405ae76b1616c6f2edf808f596438e694139f
beka <mr.begzodjon96@mail.ru>
2020-05-27 19:17:00 +0300

    User and email validation created

--- app/models/concerns/user.rb
+++ app/models/concerns/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+		validates :username, presence: true, 
+							uniqueness: {case_sensitive: false}
+		validates :email, presence: true, 
+							uniqueness: {case_sensitive: false},
+							format:{with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i}
 end
