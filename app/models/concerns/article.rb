80aa82ab45dbdea36a49e578183d16c496f201c5
beka <mr.begzodjon96@mail.ru>
2020-05-30 00:30:05 +0300

    Admin Priveliges added

--- app/models/concerns/article.rb
+++ app/models/concerns/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
    belongs_to :user
-   validates :title, presence: true, length: {minimum:3, maximum:15}
+   validates :title, presence: true, length: {minimum:3, maximum:35}
    validates :description, presence: true, length: {minimum:10, maximum:300}
    validates :user_id, presence: true
    
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_170455) do
+ActiveRecord::Schema.define(version: 2020_05_29_200725) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_170455) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
