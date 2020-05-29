d6351a0818accde3ec819c9d99dcc1b203416642
beka <mr.begzodjon96@mail.ru>
2020-05-29 22:49:53 +0300

    Sign up wll redirect to sig in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
   def create
     @user = User.new(user_params)
 
-    if @user.save
+		if @user.save
+			session[:user_id] = @user.id
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
    end
