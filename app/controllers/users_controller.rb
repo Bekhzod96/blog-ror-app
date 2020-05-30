a91c05efa0bd10d7aa503acf7e88af0d3429596c
beka <mr.begzodjon96@mail.ru>
2020-05-30 15:21:17 +0300

    Admin User permision created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@
 class UsersController < ApplicationController
 
 	before_action :set_user, only: [:edit, :update, :show]
-	before_action :require_same_user, only: [:edit, :update]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
+	before_action :require_admin, only: [:destroy]
 	
   def new
     @user = User.new
@@ -40,6 +41,12 @@ class UsersController < ApplicationController
 		@users = User.paginate(page: params[:page], per_page: 5)
 	end
 
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		flash[:danger]  = "User and all articles created by user deleted!"
+		redirect_to users_path
+	end
 	private
 	
 	def set_user
@@ -51,10 +58,17 @@ class UsersController < ApplicationController
  end
 
  def require_same_user
-		if current_user != @user
+		if current_user != @user and !current_user.admin?
 			flash[:danger] = "You can only Edit your account!"
 			redirect_to root_path
 		end
+end
+
+def require_admin
+	if logged_in?  and !current_user.admin?
+		flash[:danger] = "Only admin user can performe that action!"
+		redirect_to root_path	
+	end
 end
 
 end
