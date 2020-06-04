9eb7cb744c86938cf1aff3f722f483ef74788d85
beka <mr.begzodjon96@mail.ru>
2020-06-05 00:15:06 +0300

    Catigories_test created

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -14,5 +14,21 @@ class CategoryTest < ActiveSupport::TestCase
 		@category.name = " "
 		assert_not @category.valid?
 	end
+
+	test 'name should be unique' do
+		@category.save
+		category2 = Category.new(name: 'sports')
+		assert_not category2.valid?
+	end
+
+	test 'name should not be too long' do
+		@category.name = "a"* 26
+		assert_not @category.valid?
+	end
+
+	test "name should not be too short" do
+		@category.name = "aa"
+		assert_not @category.valid?
+	end
 	
 end
